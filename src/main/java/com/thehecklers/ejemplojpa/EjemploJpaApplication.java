568f6304237fe3b41c6cbdc97772153e87662ad8
Mark Heckler <mark.heckler@gmail.com>
2020-07-07 15:58:13 -0500

    Iniciando

---
+++ src/main/java/com/thehecklers/ejemplojpa/EjemploJpaApplication.java
@@ -0,0 +1,44 @@
+package com.thehecklers.ejemplojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class EjemploJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploJpaApplication.class, args);
+	}
+
+}
+
+@EnableScheduling
+@Component
+class PollPlaneFinder {
+	private WebClient client = WebClient.create("http://localhost:8080");
+	private final AircraftRepository repository;
+
+	public PollPlaneFinder(AircraftRepository repository) {
+		this.repository = repository;
+
+		repository.deleteAll();
+	}
+
+	@Scheduled(fixedRate = 1000)
+	private void pollPlanes() {
+		client.get()
+				.retrieve()
+				.bodyToFlux(Aircraft.class)
+				.filter(plane -> !plane.getReg().isEmpty())
+				.toStream()
+				.forEach(repository::save);
+
+		repository.findAll().forEach(System.out::println);
+	}
+}
+
+
---
+++ src/test/java/com/thehecklers/ejemplojpa/EjemploJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.ejemplojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
